9b4fb2a35cc370f80ec5e30b59a2da9d1238ad75
Tan Zen Yew <e0550503@u.nus.edu>
2021-11-08 01:15:41 +0800

    consumed web service got bug

---
+++ HorsLibrary/src/ejb/session/stateful/ReserveOperationSessionBeanRemote.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.session.stateful;
+
+import entity.PaymentEntity;
+import entity.RoomReservationEntity;
+import entity.RoomTypeEntity;
+import entity.UserEntity;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import javax.ejb.Remote;
+import util.exception.GuestNotFoundException;
+import util.exception.InvalidRoomReservationEntityException;
+import util.exception.ReservationNotFoundException;
+import util.exception.RoomTypeNotFoundException;
+import util.reservation.Pair;
+
+/**
+ *
+ * @author zenyew
+ */
+@Remote
+public interface ReserveOperationSessionBeanRemote {
+    
+    public List<Pair<RoomTypeEntity, BigDecimal>> searchRoom(int reserveType, LocalDate checkinDate, LocalDate checkoutDate, Integer numberOfRooms);
+
+    public Long makeReservation(UserEntity username, int response, PaymentEntity payment) throws RoomTypeNotFoundException, InvalidRoomReservationEntityException;
+
+    public List<RoomReservationEntity> viewAllReservation(String username) throws ReservationNotFoundException, GuestNotFoundException;
+
+    public RoomReservationEntity viewReservationDetails(Long reservationId) throws ReservationNotFoundException;
+
+    
+    
+}
